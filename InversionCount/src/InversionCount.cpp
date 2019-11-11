724c7922a99403e461a47ffbf1eba1b679990011
Maylee <mgagnon987@gmail.com>
2019-11-10 20:59:07 -0500

    1st commit

---
+++ InversionCount/src/InversionCount.cpp
@@ -0,0 +1,20 @@
+/* Maylee Gagnon
+ * CS 2223 HW3P2
+ * 11.10.19
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int easyInversionCount(){
+	return 0;
+}
+
+int fastInversionCount(){
+	return 0;
+}
+int main() {
+	cout << "Hello" << endl; // prints !!!Hello World!!!
+	return 0;
+}
