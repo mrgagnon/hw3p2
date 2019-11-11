53b795dff34c7c005defb30deeba64511fd6b09d
Maylee <mgagnon987@gmail.com>
2019-11-10 21:19:53 -0500

    simple inversion method might be working

--- InversionCount/src/InversionCount.cpp
+++ InversionCount/src/InversionCount.cpp
@@ -5,16 +5,42 @@
  */
 
 #include <iostream>
+#include <array>
 using namespace std;
 
-int easyInversionCount(){
-	return 0;
+/* O(n^2)
+ * @param arr The array of numbers to be checked
+ * @param n The number of elements in array
+ * @return The number of inversions found
+ *
+ */
+int easyInversionCount(int arr[], int n){
+	int inverCt = 0;
+	for (int i = 0; i < n-1; i++){
+		for (int j = i+1; j < n; j++){
+			if (arr[i] > arr[j]) {
+				inverCt++;
+			}
+		}
+	}
+	return inverCt;
 }
 
+/* O(nLog(n))
+ * @param arr The array of numbers to be checked
+ * @param n The number of elements in array
+ * @return The number of inversions found
+ *
+ */
 int fastInversionCount(){
 	return 0;
 }
 int main() {
-	cout << "Hello" << endl; // prints !!!Hello World!!!
+	int arr[3] = {3,2,1};
+	int n = 3;
+	cout << easyInversionCount(arr, n);
+
+	// call to fastInversionCount();
+
 	return 0;
 }
