f8b7dc5270517b41570e4179fe841c76fd0488e5
Maylee <mgagnon987@gmail.com>
2019-11-11 16:42:09 -0500

    still not working

--- InversionCount/src/InversionCount.cpp
+++ InversionCount/src/InversionCount.cpp
@@ -33,77 +33,82 @@ int easyInversionCount(int arr[], int n){
 	return inverCt;
 }
 
-/* Merges the two arrays that are previously sorted
- * @param arr The two sorted arrays to be merged
- * @param temp The resulting&merged, array with elements from the two given
- * @param lN the start of the array
- * @param mN middle position or where the first array stops
- * @param rN the end of the array
- * @return The number of inversions in the array
- */
-int merge(int arr[], int arrResult[], int lN, int mN, int rN){
-	cout << "inmerge" << endl;
-	int posLeft = lN;
-	int posRight = mN;
-	int posResult = lN;
+/*
+/* Merges two sorted array back into A
+ *
+ *
+int merge(int B[], int C[], int A[], int Bp, int Cq){
+	cout << "merge" << endl;
+	cout << "Bp: " << Bp << endl;
+
+	int i = 0;
+	int j = 0;
+	int k = 0;
 	int ct = 0;
 
-	while ((posLeft < mN) && (posRight <= rN)) {
-		if (arr[posLeft] <=  arr[posRight]) {
-			arrResult[posResult] = arr[posLeft];
-			posResult++;
-			posLeft++;
+	while ((i <= Bp) && (j <= Cq)){
+		if (B[i] <= C[j]){
+			A[k] = B[i];
+			i++;
 		}
 		else {
-			arrResult[posResult] = arr[posRight];
-			posResult++;
-			posRight++;
+			A[k] = C[j];
+			j++;
 
-			ct = ct+ (mN-1);
+			ct++;  //TODO
+		}
+		k++;
+	}
+
+	if (i == Bp) {
+		for (int x = j; x <= Cq -1; x++){ // copy C[j..q-1] to A[k..p+q-1]
+			A[k] = C[x];
+			k++;
 		}
 	}
-
-	while (posLeft < mN){
-		arrResult[posResult] = arr[posLeft];
-		posResult++;
-		posLeft++;
+	else {
+		for (int x = i; x < Bp; x++){ // copy B[i ..p-1] to A[k..p+q-1]
+			A[k] = B[x];
+			k++;
+		}
 	}
-	while (posRight <= rN) {
-		arrResult[posResult] = arr[posRight];
-		posResult++;
-		posRight++;
-	}
-
-/*
-	for (int i = lN; i <rN; i++){
-		arr[i] = arrResult[i];
-	}*/
-
 	return ct;
 }
 
-/* Uses mergeSort to sort the array
- * Divide the array, recursive call for both sides, call ot merge
- * @param arr The array to sorted and checked
- * @param arrTemp A temporary array to use during sorting
- * @param lN Starting position on the left side
- * @param rN Starting position on the right side
- * @return The number of inversions in the array
- */
-int mergeSort(int arr[], int arrTemp[], int lN, int rN){
+/*Recursive merge sort
+ * @param A Array to be sorted
+ * @param n Number of elements in array
+ * @return Number of inversions
+ *
+int mergeSort(int A[], int n){
+	cout << "mergeSort" << endl;
+	int mid = (n/2)-1;
+	int B[mid];
+	int C[mid];
 	int ct = 0;
-	if (rN > lN){
-		cout << "inmergeSort" << endl;
-		int mN = (rN+lN)/2;
-		cout << "m: " << mN << endl;
 
-		ct+= mergeSort(arr, arrTemp, lN, mN);
-		ct+= mergeSort(arr, arrTemp, mN+1, rN);
+	if (n>1){
+		cout << "mergeSort n>1" << endl;
+		for (int i = 0; i <= mid; i++){
+			B[i] = A[i];
+		}
+		int posC = 0;
+		for (int i = (mid+1); i <= n-1; i++){
+			C[posC] = A[i];
+			posC++;
+		}
 
-		ct+= merge(arr, arrTemp, lN, mN+1, rN);
+		ct += mergeSort(B, mid);
+		ct += mergeSort(C, mid);
+
+		int Bp = *(&B +1)-B;
+		int Cq = *(&C +1)-C;
+		ct += merge(B,C,A,Bp,Cq);
+		cout << "after merge call" << endl;
 	}
 	return ct;
 }
+*/
 
 /* Suppose to be done in O(nLog(n))
  * Merge sort & count, prints sorted array
@@ -114,10 +119,10 @@ int mergeSort(int arr[], int arrTemp[], int lN, int rN){
  */
 int fastInversionCount(int arr[], int size){
 	cout << "in fastInversionCount" << endl;
-	int tempArr[size];
-	int i = mergeSort(arr, tempArr, 0, size-1);
-	cout << "tempArr: " << endl;
-	printArr(tempArr, size);
+
+	int i = mergeSort(arr,size);
+	cout << "arr: " << endl;
+	printArr(arr, size);
 
 	return i;
 }
@@ -129,10 +134,14 @@ int main() {
 
 
 	// call to fastInversionCount();
-	int testArr[4] = {4,2,3,1};
-	int n1 = 4; // TODO calculate size
+	int testArr[2] = {4,2};
+	int n1 = 2; // TODO calculate size
+	//int testArr[4] = {4,2,3,1};
+	//int n1 = 4; // TODO calculate size
 	//int testArr[8] = {19,8,90,7,6,1,3,11};
 	//int n1 = 8;
+	//int testArr[4] = {1,2,3,4};
+	//int n1 = 4;
 	cout << "orginal:" << endl;
 		printArr(testArr,n1);
 
